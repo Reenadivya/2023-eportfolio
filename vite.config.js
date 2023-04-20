be0f54fd5cb5e1f2c5f4dbae097dc0b0377025c5
Reenadivya <reenad286@gmail.com>
2023-04-20 14:09:09 +0800

    changed config file

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: {
-        main: "./index.html",
-      },
-      external: [
-        "react-router-dom",
-        // Add other external modules here if needed
-      ],
+      external: ["react-router-dom", "@fortawesome/react-fontawesome"],
     },
   },
 });
