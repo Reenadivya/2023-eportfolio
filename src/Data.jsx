6bae54be32c2a37f5f87ce5f1bb0beab8ccc055d
Reenadivya <reenad286@gmail.com>
2023-04-20 22:51:14 +0800

    added images

--- src/Data.jsx
+++ src/Data.jsx
@@ -4,7 +4,7 @@ export const projects = [
     title: "Virtual Internship for Frontend Simplified",
     description:
       "Created a responsive NFT Marketplace with skeleton loading states, countdown timers, and dyanamic routing.",
-    url: "https://storage.cloud.google.com/portfolio-image/assets/Virtual%20Internship_NFT%20Marketplace.png",
+    url: "./assets/Virtual Internship_NFT Marketplace.png",
     languages: "React, JavaScript, HTML, CSS, Bootstrap",
     link: "https://reena-fes-internship.vercel.app",
   },
@@ -13,7 +13,7 @@ export const projects = [
     title: "Magic Eden Creator Monetization Hackathon Submission 2022",
     description:
       "Created an NFT royalty tracking tool for the Solana ecosystem using Heliu's API.",
-    url: "https://storage.cloud.google.com/portfolio-image/assets/Magic_Eden_RoyaltyTracker.png",
+    url: "./assets/Magic_Eden_RoyaltyTracker.png",
     languages: "React, JavaScript, HTML, CSS",
     link: "https://liberty-royalty.netlify.app",
   },
@@ -22,7 +22,7 @@ export const projects = [
     title: "Glamour Boutique Ecommerce Store",
     description:
       "Created an NFT royalty tracking tool for the Solana ecosystem using Helius's API.",
-    url: "https://storage.cloud.google.com/portfolio-image/assets/Glamour%20Boutique%20Ecommerce%20Site%20React.png",
+    url: "./assets/Glamour Boutique Ecommerce Site React.png",
     languages: "React, Redux, JavaScript, HTML, CSS",
     link: "https://glamourboutique.netlify.app",
   },
@@ -31,7 +31,7 @@ export const projects = [
     title: "Cinemascope - A One-Stop Movie App Experience",
     description:
       "Cinemascope, a cutting-edge movie app that offers an unparalleled, immersive experience to movie enthusiasts.",
-    url: "https://storage.cloud.google.com/portfolio-image/assets/Advanced_Portfolio_Cinemascope.png",
+    url: "./assets/Advanced_Portfolio_Cinemascope.png",
     languages: "React, JavaScript, HTML, CSS",
     link: "https://cinemascope-movieapp-reena.netlify.app",
   },
@@ -40,7 +40,7 @@ export const projects = [
     title: "Book Haven - Library Store",
     description:
       "Book Haven is a library that showcases a range of books. Customers can rank books accoring to the highest rating or price. This application also have add to cart functionality and a skeleton loading state to improve the UI and UX. Dynamically pulled data from a database and mapped into each section.",
-    url: "https://storage.cloud.google.com/portfolio-image/assets/Book%20Haven_React_proj.png",
+    url: "./assets/Book Haven_React_proj.png",
     link: "https://cinemascope-movieapp-reena.netlify.app",
   },
   {
@@ -48,7 +48,7 @@ export const projects = [
     title: "Cinemascope",
     description:
       "Created a responsive NFT Marketplace with skeleton loading states, countdown timers, and dyanamic routing.",
-    url: "https://storage.cloud.google.com/portfolio-image/assets/Advanced_Portfolio_JSProject.png",
+    url: "./assets/Advanced_Portfolio_JSProject.png",
     languages: "JavaScript, HTML, CSS",
     link: "https://cinemascope-portfolio.netlify.app",
   },
@@ -57,7 +57,7 @@ export const projects = [
     title: "Rick and Morty Wiki",
     description:
       "Explore the Rick and Morty universe with our JavaScript wiki, powered by the Rick and Morty API. Our user-friendly interface allows you to easily search for and discover characters, episodes, and locations from the show.",
-    url: "https://storage.cloud.google.com/portfolio-image/assets/Rick%20and%20Morty%20Wiki%20JS%20Proj.png",
+    url: "./assets/Rick and Morty Wiki JS Proj.png",
     languages: "JavaScript, HTML, CSS",
     link: "https://rickmortywiki-reena.netlify.app",
   },
@@ -66,7 +66,7 @@ export const projects = [
     title: "Agency Landing Page",
     description:
       "Experience a seamless and user-friendly web application for mock agency services, built with JavaScript. Our app boasts a modern and visually appealing design that adapts to all screen sizes and devices.",
-    url: "https://storage.cloud.google.com/portfolio-image/assets/Edie_Project_JS.png",
+    url: "./assets/Edie_Project_JS.png",
     languages: "JavaScript, HTML, CSS",
     link: "https://rickmortywiki-reena.netlify.app",
   },
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        "react()",
-        "react-router-dom",
-        "@fortawesome/react-fontawesome",
-        "@fortawesome/free-solid-svg-icons",
-        "@fortawesome/free-brands-svg-icons",
-        "@emailjs/browser",
-        "aos",
-        "aos/dist/aos.css",
-      ],
-    },
-  },
 });
